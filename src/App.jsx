f71d8ca46b8e30ea42aab610e8c6abf6a563021b
zhangyang <zhangyang@inke.cn>
2021-12-13 20:03:59 +0800

    修改vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { useState } from 'react'
 import './App.css'
 import Pages from './pages/Pages'
 import {Link,BrowserRouter as Router,Routes, Route } from 'react-router-dom'
-import About from './pages/About'
-
+import About from '@/pages/About'
+import {Layout} from 'antd'
+import 'antd/dist/antd.css'; 
+const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-        <Link to="/">pages</Link>
-        <Link to="/about">about</Link>
-        </header>
-        <Routes>
-          <Route path='/'  element={<Pages></Pages>} />
-          <Route path='/about' element={<About></About>} />
-        </Routes>
-     
-      </div>
-     
+      <Layout style={{height: '100%'}}>
+        <Sider>
+          <Link to="/">pages</Link>
+          <Link to="/about">about</Link>
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content>
+            <Routes>
+              <Route path='/'  element={<Pages></Pages>} />
+              <Route path='/about' element={<About></About>} />
+            </Routes>
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
     </Router>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  alias:{
+    '@':path.resolve(__dirname, './src')
+  },
+  server:{
+    port: 4000
+  },
+  proxy:{
+    '/api':{
+      target: path.resolve(__dirname, '')
+    }
+  }
 })
